cb35f24f5c88b11f1bf1f885bf52e9b737e94b4d
vickydasta <vickydasta@gmail.com>
2016-07-22 23:28:33 +0700

    changed port

--- apps.py
+++ apps.py
@@ -181,7 +181,6 @@ def before_request():
     g.user = current_user
 
 if __name__ == "__main__":
-    db.create_all()
     app.debug = True
     port = int(os.environ.get('PORT', 5000))
     app.run(host="0.0.0.0", port=port, threaded=True)
